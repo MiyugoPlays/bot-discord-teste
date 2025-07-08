59b000e0da5245bfbdbcaa8fbb1892e510005d2e
MiyugoPlays <miyugoplays@gmail.com>
2025-07-08 11:33:19 -0300

    adição de comandos adicionais no /ping (maioria tudo comentado) e implantado localização da mensagem no user

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,11 +1,30 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
-    cooldown: 5,
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong!')
+        //await interaction.deferReply(); //deferReply serve pra mandar uma msg de delay pro discord pensar
+        //await wait(4_000);              //ja o editReply edita o reply
+        //await interaction.editReply('Pong!')
+
+
+        await interaction.reply('Pongui!')
+        await wait(2_000);
+        await interaction.followUp({ content: 'Pong secreto (hoi)', flags: MessageFlags.Ephemeral }); //followup serve pra mandar uma mensagem logo em seguida
+        //{ content: 'Pong secreto', flags: MessageFlags.Ephemeral } essa parte de ephemeral serve pra mandar msg unica pro user
+
+        //await interaction.reply('Pongui');
+        //await wait(2_000);
+        //await interaction.deleteReply(); //deleta o reply
+
+        //const response = await interaction.reply({ content: 'Pong!', withResponse: true })
+        //console.log(response.resource.message); // basicamente com o withResponse vc tem um log do comando
+
+        //await interaction.reply('Pongui!'); 
+        //const message = await interaction.fetchReply();
+        //console.log(message); //msm função que o anterior praticamente  
     },
 };
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -5,6 +5,13 @@ module.exports = {
         .setName('user')
         .setDescription('Provides information about the user. '),
     async execute(interaction) {
-        await interaction.reply(`This comando foi rodado pelo ${interaction.user.username}, que logou ${interaction.member.joinedAt}.`);
+        const locales = {
+                'en-US': `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
+                'es-ES': `Este comando fue ejecutado por ${interaction.user.username}, quien se unió el ${interaction.member.joinedAt}.`,
+                fr: `Cette commande a été exécutée par ${interaction.user.username}, qui a rejoint le ${interaction.member.joinedAt}.`,
+                ja: `${interaction.user.username} がこのコマンドを実行しました。参加日時: ${interaction.member.joinedAt}。`,
+        };
+
+        await interaction.reply(locales[interaction.locale] ?? `Esse comando foi rodado pelo ${interaction.user.username}, que logou ${interaction.member.joinedAt}.`);
     },
 }
